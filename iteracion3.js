7069b25cc976279b887bcf8445ac06bc1cd5637b
Alfonso Cifuentes <alfonsocifuentes83@gmail.com>
2021-11-03 15:48:17 +0100

    Iteration#3

---
+++ iteracion3.js
@@ -0,0 +1,14 @@
+//Iteración #3: Probando For.. of
+
+const placesToTravel = [
+  "Japon",
+  "Venecia",
+  "Murcia",
+  "Santander",
+  "Filipinas",
+  "Madagascar",
+];
+
+for (value of placesToTravel) {
+  console.log(value);
+}
